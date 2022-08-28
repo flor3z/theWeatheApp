544b3a6a6707a5abaae29817df01aef97da368df
Matheo <mflorez3@live.com>
2022-08-28 01:56:03 -0400

    5th: frequent styling errs -- trying for API call - small win with API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 // import Container from '@mui/material/Container';
+import TextField from '@mui/material/TextField';
 import Paper from '@mui/material/Paper';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
@@ -9,35 +10,62 @@ import Grid from '@mui/material/Grid';
 // import Image from './images/background.jpg';
 
 function App() {
-  // const url = 'https://api.openweathermap.org/data/2.5/weather?lat=43.70011&lon=-79.4163&appid=8f04bd3cf5e9e3a13c6c3ec1a326036a';
+  const [data, setData] = useState({});
+  const [position, setPosition] = useState('');
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${position}&appid=8f04bd3cf5e9e3a13c6c3ec1a326036a`;
+
+  const searchPosition = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      });
+    }
+  };
 
   return (
     <Paper>
-      <AppBar color="primary" position="static" style={{ height: '5rem' }}>
+      <AppBar
+        className="header"
+        color="primary"
+        position="static"
+        style={{ height: '7rem' }}
+      >
         <Toolbar>
-          <Typography color="inherit">The Weather App</Typography>
+          <Typography color="inherit" fontSize={'2rem'}>
+            The Weather App
+          </Typography>
+          <TextField
+            value={position}
+            onKeyPress={searchPosition}
+            onChange={(event) => setPosition(event.target.value)}
+            id="outlined-basic"
+            label="Outlined"
+            variant="outlined"
+          />
         </Toolbar>
       </AppBar>
       <Grid
         className="upper-container"
-        // marginTop={'4rem'}
+        paddingTop={'4rem'}
         justifyContent="center"
         alignItems="center"
         direction="column"
         container
         // background={Image}
       >
-        <Grid item marginTop="2rem" className="upper">
-          <Grid item className="upper__location">
-            <p>Toronto</p>
-          </Grid>
-          <Grid item className="upper__temp">
-            <h1>30° C</h1>
-          </Grid>
-          <Grid item className="upper__description">
-            <p>Clear Skies and Sunny</p>
-          </Grid>
+        {/* <Grid item marginTop="2rem" className="upper"> */}
+        <Grid item className="upper__location">
+          <p>Toronto</p>
         </Grid>
+        <Grid item className="upper__temp">
+          <h1>30° C</h1>
+        </Grid>
+        <Grid item className="upper__description">
+          <p>Clear Skies and Sunny</p>
+        </Grid>
+        {/* </Grid> */}
         <Grid
           container
           marginTop="15rem"
