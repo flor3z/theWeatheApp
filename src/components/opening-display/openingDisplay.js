a5ca524f31afa50b577f388814ded6082e9dbb82
Matheo <mflorez3@live.com>
2022-10-11 19:33:49 -0400

    More Styling for Opening Page - still need to fix title for mobile size screens

--- src/components/opening-display/openingDisplay.js
+++ src/components/opening-display/openingDisplay.js
@@ -1,13 +1,19 @@
 const OpeningDisplay = () => {
   return (
     <div className="opening">
-      <div className="opening__content-container">
+      <div className="opening__image-container">
         <img
           alt="weather-icon"
           className="opening__weather-icon"
           src={'icons/weatherIcon.png'}
         />
-        <h1>Search Your Local Forecast</h1>
+      </div>
+
+      <div className="opening__title-container">
+        <span className="opening__title-container__word-odd">Search</span>
+        <span className="opening__title-container__word-even">Your</span>
+        <span className="opening__title-container__word-odd">Local</span>
+        <span className="opening__title-container__word-even">Forecast</span>
       </div>
     </div>
   );
